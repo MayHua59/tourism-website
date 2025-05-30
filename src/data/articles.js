c3bd3a2910a655302aae9ee978342ee5c9738240
Cisco Ramon <aungkoman@gmail.com>
2025-05-30 19:03:37 +0630

    image url updated

--- src/data/articles.js
+++ src/data/articles.js
@@ -3,7 +3,7 @@ export const articlesData = [
     id: "article1",
     title: "Top 10 Must-Visit Pagodas in Myanmar",
     summary: "Explore the most iconic and breathtaking pagodas across Myanmar, rich in history and spiritual significance.",
-    image_url: "https://images.unsplash.com/photo-1552098479-bddc0d0a4119?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFnYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+    image_url: "https://images.unsplash.com/photo-1584412370502-1dd6d55d1060?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     url: "/articles/top-10-pagodas-myanmar",
     relatedCities: ["bagan", "yangon", "mandalay"] 
   },
@@ -11,7 +11,7 @@ export const articlesData = [
     id: "article2",
     title: "A Culinary Journey Through Yangon's Street Food",
     summary: "Dive into the vibrant street food scene of Yangon and discover delicious local delicacies you can't miss.",
-    image_url: "https://images.unsplash.com/photo-1599600300922-7d67d86cef07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8eWFuZ29uJTIwc3RyZWV0JTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    image_url: "https://images.unsplash.com/photo-1584412370502-1dd6d55d1060?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     url: "/articles/yangon-street-food-guide",
     relatedCities: ["yangon"]
   },
@@ -19,7 +19,7 @@ export const articlesData = [
     id: "article3",
     title: "Trekking in Kalaw: A Guide to Myanmar's Hill Country",
     summary: "Experience the stunning landscapes and unique cultures of Myanmar's Shan State with an unforgettable trek from Kalaw.",
-    image_url: "https://images.unsplash.com/photo-1604724955313-15de3356406e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8a2FsYXclMjB0cmVra2luZ3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    image_url: "https://images.unsplash.com/photo-1584412370502-1dd6d55d1060?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     url: "/articles/trekking-kalaw-guide",
     relatedCities: ["kalaw", "shan state"] 
   }
