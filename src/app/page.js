292a29866d27e35382ce680556fdd3ac37c9eda6
MayHua59 <mayhuadev59@gmail.com>
2025-06-02 10:34:45 +0630

    fix bug by removing @/ for component routes

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import ArticleSection from "@/components/ArticleSection.jsx";
+import ArticleSection from "../components/ArticleSection";
 import Carousel from "../components/Carousel";
 import HeroSection from "../components/HeroSection";
-import DestinationByRegion from "@/components/DestinationByRegion";
+import DestinationByRegion from "../components/DestinationByRegion";
 import Image from "next/image";
-import EventsSection from "@/components/EventSection";
-import CultureSection from "@/components/CultureSection";
+import EventsSection from "../components/EventSection";
+import CultureSection from "../components/CultureSection";
 
 export default function Home() {
   return (
