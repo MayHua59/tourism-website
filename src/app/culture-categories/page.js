ee46854a60aaf10019e9d4e7e90c28b1baa2c1be
MayHua59 <mayhuadev59@gmail.com>
2025-06-03 12:18:31 +0630

    implement CultureCategoryList page

---
+++ src/app/culture-categories/page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CultureCategoryListPage from '../../main-components/CultureCategoryList/CultureCategoryList';
+
+const Page = () => {
+  return (
+    <CultureCategoryListPage/>
+  )
+}
+
+export default Page
